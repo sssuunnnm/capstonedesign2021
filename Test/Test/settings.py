f4b92ea290a60b40e2fc54afb14f45483dbec112
JayDwk <83630504+JayDwk@users.noreply.github.com>
2021-11-03 17:51:25 +0900

    Add files via upload

--- Test/Test/settings.py
+++ Test/Test/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'Test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gabaechuDB'
+                '',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
