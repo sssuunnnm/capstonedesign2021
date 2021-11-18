2a48567bec14f23dab21e8a39a99f6037022202b
JayDwk <83630504+JayDwk@users.noreply.github.com>
2021-11-18 15:18:30 +0900

    Add files via upload

--- Test/polls/models.py
+++ Test/polls/models.py
@@ -1,13 +1,16 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 class user_info(models.Model):
-    AGE_TEN = 10
-    AGE_TWENTY = 20
-    AGE_THIRTY = 30
-    AGE_FORTY = 40
-    AGE_CHOICES = ((AGE_TEN, 10), (AGE_TWENTY, 20), (AGE_THIRTY, 30), (AGE_FORTY, 40))
+
+    #AGE_TEN = 10
+    #AGE_TWENTY = 20
+    #AGE_THIRTY = 30
+    #AGE_FORTY = 40
+    #AGE_CHOICES = ((AGE_TEN, 10), (AGE_TWENTY, 20), (AGE_THIRTY, 30), (AGE_FORTY, 40))
 
     GENDER_MALE = "남"
     GENDER_FEMALE = "여"
@@ -19,22 +22,26 @@ class user_info(models.Model):
     JOB_WORKER = "직장인"
     JOB_CHOICES = ((JOB_STUDENT, "학생"), (JOB_FREELANCE, "프리랜서"), (JOB_NONE, "무직"), (JOB_WORKER, "직장인"))
 
-    #user_id = models.CharField(max_length=100, default='', db_column='아이디')
-    age = models.IntegerField(choices=AGE_CHOICES, db_column='나이')
+    user = models.ForeignKey(User, default='', on_delete=models.CASCADE)
+    year = models.IntegerField(db_column='년')
+    month = models.IntegerField(db_column='월')
+    day = models.IntegerField(db_column='일')
     gender = models.CharField(choices=GENDER_CHOICES, max_length=5, db_column='성별')
     job = models.CharField(choices=JOB_CHOICES, max_length=20, db_column='직업')
-    room_name = models.CharField(max_length=50, db_column='방이름')
-    room_size = models.IntegerField(db_column='방크기')
+    room_name = models.CharField(max_length=50, db_column='방이름', null=True)
+    room_size = models.IntegerField(db_column='방크기', null=True)
 
 
 class Fixed_info(models.Model):
-    user_id = models.ForeignKey('user_info', default='', on_delete=models.CASCADE, db_column='아이디')
+    user = models.ForeignKey(User, default='', on_delete=models.CASCADE)
     kitchen = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='주방')
-    frige = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='냉장고')
+    fridge = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='냉장고')
     toilet = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='화장실')
     bed = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='침대')
     closet = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='옷장')
     washer = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='세탁기')
+    front = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='현관')
+
 
 
 class Add_info(models.Model):
@@ -43,9 +50,9 @@ class Add_info(models.Model):
     MATTRESS_QUEEN = "퀸"
     MATTRESS_KING = "킹"
     MATTRESS_CHOICE = ((MATTRESS_SINGLE, "싱글"), (MATTRESS_SUPER, "슈퍼싱글"), (MATTRESS_QUEEN, "퀸"), (MATTRESS_KING, "킹"))
-    user_id = models.ForeignKey('user_info', on_delete=models.CASCADE, db_column='아이디')
+    user = models.ForeignKey(User, default='', on_delete=models.CASCADE)
     mattress = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='매트리스')
-    frige = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='냉장고')
+    fridge = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='냉장고')
     washer = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='세탁기')
     desk = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='책상')
     closet = models.PositiveIntegerField(validators=[MaxValueValidator(25)], db_column='옷장')
